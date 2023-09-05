1c4d6ac17c94fceb5a61d3c33bcb71cc98863770
Robert Mikolajczyk <ronert1674@gmail.com>
2023-09-05 22:51:05 +0200

    improves user input asking if continue adding new bidders

--- main.py
+++ main.py
@@ -14,10 +14,18 @@ def update_user_info(name: str, bid_price: int) ->None:
     
 def main():
     os.system('clear')
-    print(logo)
-    name = input("What is your name? ")
-    bid_price = int(input("What is your bid price? $"))
-    update_user_info(name=name, bid_price=bid_price)
+    should_continue = True
+    while should_continue:
+        print(logo)
+        name = input("What is your name? ")
+        bid_price = int(input("What is your bid price? $"))
+        update_user_info(name=name, bid_price=bid_price)
+        is_next_user = input("Are there other users who want to bid? 'yes' or 'no'? ")
+        if is_next_user == 'no':
+            should_continue = False
+        else:
+            os.system('clear')
+
     print(user_info)
 
 
