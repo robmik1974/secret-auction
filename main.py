5ede91dcc16e01cb3ab67fd6fe5c14a0df883864
Robert Mikolajczyk <ronert1674@gmail.com>
2023-09-06 07:46:01 +0200

    refactors the code

--- main.py
+++ main.py
@@ -2,31 +2,45 @@ from art import logo
 import os
 
 
-user_info = list()
+bid = list()
 
 
-def update_user_info(name: str, bid_price: int) ->None:
+def add_new_bidder(name: str, bid_price: int) ->dict[str, int]:
     user_data = dict()
     user_data["name"] = name
     user_data["bid_price"] = bid_price
-    user_info.append(user_data)
+    return user_data
     
+
+def find_the_highest_bidder(bid: dict[str, int]) ->tuple[str, int]:
+    highest_bid = 0
+    winner = ""
+    for bidder in bid:
+        if bidder["bid_price"] > highest_bid:
+            highest_bid = bidder["bid_price"]
+            winner = bidder["name"]
     
+    return winner, highest_bid
+
+
 def main():
     os.system('clear')
     should_continue = True
     while should_continue:
         print(logo)
         name = input("What is your name? ")
-        bid_price = int(input("What is your bid price? $"))
-        update_user_info(name=name, bid_price=bid_price)
-        is_next_user = input("Are there other users who want to bid? 'yes' or 'no'? ")
+        bid_price = int(input("What is your bid price?: $"))
+        bid.append(add_new_bidder(name=name, bid_price=bid_price))
+        is_next_user = input("Are there other users who want to bid? 'yes' or 'no'?:\n")
         if is_next_user == 'no':
             should_continue = False
         else:
             os.system('clear')
 
-    print(user_info)
+    winner_name, winner_price = find_the_highest_bidder(bid)
+    os.system('clear')
+    print(logo)
+    print(f"The winner is {winner_name} who paid ${winner_price}")
 
 
 if __name__ == "__main__":
